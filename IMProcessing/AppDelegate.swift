eb87e94bb0cb24bb06a71a99a494e226680583c1
denis svinarchuk (book pro '15year) <denn.nevera@gmail.com>
2015-12-21 19:43:53 +0300

    add example

--- IMProcessing/AppDelegate.swift
+++ IMProcessing/AppDelegate.swift
@@ -1,26 +1,150 @@
 //
 //  AppDelegate.swift
-//  IMProcessing
+//  ImageMetalling-07
 //
-//  Created by denis svinarchuk on 21.12.15.
-//  Copyright © 2015 Dehancer.photo. All rights reserved.
+//  Created by denis svinarchuk on 14.12.15.
+//  Copyright © 2015 IMetalling. All rights reserved.
 //
 
 import Cocoa
 
 @NSApplicationMain
+
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+    
+    
+    private let openRecentKey = "imageMetalling-open-recent"
+    
+    private var openRecentMenuItems = Dictionary<String,NSMenuItem>()
+    
+    private func addOpenRecentMenuItemMenu(file:String){
+        let menuItem = openRecentMenu.insertItemWithTitle(file, action: "openRecentHandler:", keyEquivalent: "", atIndex: 0)
+        openRecentMenuItems[file]=menuItem
+    }
+    
+    private var openRecentList:[String]?{
+        get {
+            return NSUserDefaults.standardUserDefaults().objectForKey(openRecentKey) as? [String]
+        }
     }
 
+    
+    private func addOpenRecentFileMenuItem(file:String){
+
+        var list = removeOpenRecentFileMenuItem(file)
+        
+        list.insert(file, atIndex: 0)
+        
+        NSUserDefaults.standardUserDefaults().setObject(list, forKey: openRecentKey)
+        NSUserDefaults.standardUserDefaults().synchronize()
+        
+        addOpenRecentMenuItemMenu(file)
+    }
+    
+    private func removeOpenRecentFileMenuItem(file:String) -> [String] {
+
+        var list = openRecentList ?? [String]()
+        
+        if let index = list.indexOf(file){
+            list.removeAtIndex(index)
+            if let menuItem = openRecentMenuItems[file] {
+                openRecentMenu.removeItem(menuItem)
+            }
+        }
+        
+        NSUserDefaults.standardUserDefaults().setObject(list, forKey: openRecentKey)
+        NSUserDefaults.standardUserDefaults().synchronize()
+
+        return list
+    }
+    
+    private var recentListMenuItems:[NSMenuItem] = [NSMenuItem]()
+    
+    @IBOutlet weak var openRecentMenu: NSMenu!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        
+        for w in NSApp.windows{
+            w.backgroundColor = IMPColor(color: IMPPrefs.colors.background)
+        }
+        
+        if let list = openRecentList {
+            for file in list.reverse() {
+                addOpenRecentMenuItemMenu(file)
+            }
+            IMPDocument.sharedInstance.currentFile = list[0]
+        }
+    }
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
+    @IBAction func menuAction(sender: NSMenuItem) {
+        IMPMenuHandler.sharedInstance.currentMenuItem = sender
+    }
+    
+    
+    @IBAction func clearRecentOpened(sender: AnyObject) {
+        if let list = openRecentList {
+            for file in list {
+                removeOpenRecentFileMenuItem(file)
+            }
+        }
+    }
+    
+    @IBAction func openFile(sender: AnyObject) {
+        
+        let openPanel = NSOpenPanel()
+        
+        openPanel.canChooseFiles  = true;
+        openPanel.resolvesAliases = true;
+        openPanel.extensionHidden = false;
+        openPanel.allowedFileTypes = ["jpg", "tiff", "png"]
+        
+        let result = openPanel.runModal()
+        
+        if result == NSModalResponseOK {
+            self.openRecentListAdd(openPanel.URLs)
+        }
+    }
+    
+    
+    @IBAction func loadLuteFile(sender: AnyObject) {
+        let openPanel = NSOpenPanel()
+        
+        openPanel.canChooseFiles  = true;
+        openPanel.resolvesAliases = true;
+        openPanel.extensionHidden = false;
+        openPanel.allowedFileTypes = ["cube", "CUBE", "Cube"]
+        
+        let result = openPanel.runModal()
+        
+        if result == NSModalResponseOK {
+            IMPDocument.sharedInstance.currentLutFile=openPanel.URLs[0].path
+        }
+    }
+    
+    func openRecentHandler(sender:NSMenuItem){
+        self.openFilePath(sender.title)
+    }
+    
+    private func openFilePath(filePath: String){
+        IMPDocument.sharedInstance.currentFile = filePath
+        addOpenRecentFileMenuItem(filePath)
+    }
+    
+    private func openRecentListAdd(file:String){
+        self.openFilePath(file)
+    }
+    
+    private func openRecentListAdd(urls:[NSURL]){
+        for url in urls{
+            if let file = url.path{
+                self.openRecentListAdd(file)
+            }
+        }
+    }
 }
 
---
+++ IMProcessing/IMPTestFilter.swift
@@ -0,0 +1,69 @@
+//
+//  IMPTestFilter.swift
+//  ImageMetalling-07
+//
+//  Created by denis svinarchuk on 19.12.15.
+//  Copyright © 2015 IMetalling. All rights reserved.
+//
+
+import Cocoa
+
+
+class IMPTestFilter:IMPFilter {
+    
+    var sourceAnalayzer:IMPHistogramAnalyzer!
+    var dominantAnalayzer:IMPHistogramAnalyzer!
+    
+    let dominantSolver = IMPHistogramDominantColorSolver()
+    let rangeSolver = IMPHistogramRangeSolver()
+    
+    var wbFilter:IMPWBFilter!
+    var contrastFilter:IMPContrastFilter!
+    
+    required init(context: IMPContext, histogramView:IMPView, histogramCDFView:IMPView) {
+        
+        super.init(context: context)
+        
+        addFunction(IMPFunction(context: self.context, name: IMPSTD_PASS_KERNEL))
+        
+        wbFilter = IMPWBFilter(context: self.context)
+        contrastFilter = IMPContrastFilter(context: self.context)
+        
+        addFilter(contrastFilter)
+        addFilter(wbFilter)
+
+        dominantAnalayzer = IMPHistogramAnalyzer(context: self.context)
+        dominantAnalayzer.solvers.append(dominantSolver)
+        
+        sourceAnalayzer = IMPHistogramAnalyzer(context: self.context)
+        sourceAnalayzer.solvers.append(rangeSolver)
+        
+        sourceAnalayzer.addUpdateObserver({ (histogram) -> Void in
+            self.contrastFilter.adjustment.minimum = self.rangeSolver.minimum
+            self.contrastFilter.adjustment.maximum = self.rangeSolver.maximum
+        })
+        
+        dominantAnalayzer.addUpdateObserver { (histogram) -> Void in
+            self.wbFilter.adjustment.dominantColor = self.dominantSolver.color
+        }
+        
+        contrastFilter.addDestinationObserver { (destination) -> Void in
+            self.dominantAnalayzer.source = destination
+        }
+        
+        addSourceObserver { (source) -> Void in
+            self.sourceAnalayzer.source = source
+        }        
+        
+        addDestinationObserver { (destination) -> Void in
+            histogramView.source = destination
+            histogramCDFView.source = destination
+        }
+    }
+    
+    required init(context: IMPContext) {
+        fatalError("init(context:) has not been implemented")
+    }
+    
+    var redraw: (()->Void)?
+}
