233383476c15800b87a74fcbc8b74812b6e071c9
denis svinarchuk (book pro '15year) <denn.nevera@gmail.com>
2016-02-16 18:39:55 +0300

    new documented comments awb api updated

--- IMProcessing/Classes/Adjustments/IMPAutoWBFilter.swift
+++ IMProcessing/Classes/Adjustments/IMPAutoWBFilter.swift
@@ -12,36 +12,51 @@ import Metal
 /// Should return opacity value
 public typealias IMPAutoWBAnalyzeHandler =  ((solver:IMPColorWeightsSolver, opacity:Float, wbFilter:IMPWBFilter, hsvFilter:IMPHSVFilter) -> Float)
 
+///  @brief AWB adjustment
 public struct IMPAutoWBAdjustment{
     public var blending:IMPBlending = IMPBlending(mode: IMPBlendingMode.NORMAL, opacity: 1)
 }
 
-public struct IMPAutoWBPreferences{
-    public var threshold  = Float(0.25)
-    public var clipping   = IMPColorWeightsClipping(white: 0.05, black: 0.1, saturation: 0.07)
-    public var hsvProfile = IMPHSVAdjustment(
-        master: IMPHSVLevel.init(hue: 0, saturation: 0, value: 0),
-        levels: (
-            IMPHSVLevel(hue: 0, saturation:  0,     value: 0),
-            IMPHSVLevel(hue: 0, saturation:  0,     value: 0), // descrease yellows in default AWB profile
-            IMPHSVLevel(hue: 0, saturation:  0,     value: 0),
-            IMPHSVLevel(hue: 0, saturation:  0,     value: 0),
-            IMPHSVLevel(hue: 0, saturation:  0,     value: 0),
-            IMPHSVLevel(hue: 0, saturation:  0,     value: 0)),
-        blending: IMPBlending.init(mode: IMPBlendingMode.NORMAL, opacity: 1))
-}
-
+///
+/// Auto white balance and color correction filer.
+/// TThe main idea based on seeking a dominant color of the image and 
+/// rearrange gamma of the image according to this dominant color. The image becames neutral-balanced.
+///
 public class IMPAutoWBFilter:IMPFilter{
-    
-    public var preferences:IMPAutoWBPreferences = IMPAutoWBPreferences() {
+
+    ///  @brief Correction preferences
+    public struct Preferences{
+        /// Neutrals threshold
+        /// In case when source analizer detects neutrals colors within area has weight greater then 25%
+        /// AWB opacity becomes equal 0
+        public var threshold  = Float(0.25)
+        /// Clip whites and blacks regions
+        public var clipping   = IMPColorWeightsClipping(white: 0.05, black: 0.1, saturation: 0.07)
+        /// Color correction profile based in HSV
+        public var hsvProfile = IMPHSVAdjustment(
+            master: IMPHSVLevel.init(hue: 0, saturation: 0, value: 0),
+            levels: (
+                IMPHSVLevel(hue: 0, saturation:  0,     value: 0),
+                IMPHSVLevel(hue: 0, saturation:  0,     value: 0), // descrease yellows in default AWB profile
+                IMPHSVLevel(hue: 0, saturation:  0,     value: 0),
+                IMPHSVLevel(hue: 0, saturation:  0,     value: 0),
+                IMPHSVLevel(hue: 0, saturation:  0,     value: 0),
+                IMPHSVLevel(hue: 0, saturation:  0,     value: 0)),
+            blending: IMPBlending.init(mode: IMPBlendingMode.NORMAL, opacity: 1))
+    }
+
+    /// AWB corrction preferences
+    public var preferences:Preferences = Preferences() {
         didSet{
             colorWeightsAnalyzer.clipping = preferences.clipping
             self.dirty = true
         }
     }
     
+    /// Default adjustment
     public static var defaultAdjustment = IMPAutoWBAdjustment(blending: IMPBlending.init(mode: IMPBlendingMode.NORMAL, opacity: 1))
     
+    /// Current adjustment
     public var adjustment:IMPAutoWBAdjustment!{
         didSet{
             wbFilter.adjustment.blending = adjustment.blending
@@ -49,25 +64,31 @@ public class IMPAutoWBFilter:IMPFilter{
         }
     }
     
+    /// Detected color weights
     public var colorWeights:IMPColorWeightsSolver.ColorWeights?{
         get{
             return colorWeightsAnalyzer.solver.colorWeights
         }
     }
     
+    /// Detected neutral weights
     public var neutralWeights:IMPColorWeightsSolver.NeutralWeights?{
         get{
             return colorWeightsAnalyzer.solver.neutralWeights
         }
     }
     
+    /// Detected dominant color
     public var dominantColor:float4?{
         get{
             return dominantColorSolver.color
         }
     }
     
-    /// User defined color analyze extention
+    ///
+    /// User defined color analyze extention.
+    /// It can be used to redefine color correction prefrences at runtime
+    ///
     public var colorsAnalyzeHandler:IMPAutoWBAnalyzeHandler?
     
     public required init(context: IMPContext, optimization:IMPHSVFilter.optimizationLevel) {
--- IMProcessing/Classes/Adjustments/IMPWBFilter.swift
+++ IMProcessing/Classes/Adjustments/IMPWBFilter.swift
@@ -9,13 +9,20 @@
 import Foundation
 import Metal
 
+/// White balance correction filter
 public class IMPWBFilter:IMPFilter,IMPAdjustmentProtocol{
     
+    /// Default WB adjustment
     public static let defaultAdjustment = IMPWBAdjustment(
+        ///  @brief default dominant color of the image
+        ///
         dominantColor: float4([0.5, 0.5, 0.5, 0.5]),
+        ///  @brief Blending mode and opacity
+        ///
         blending: IMPBlending(mode: IMPBlendingMode.NORMAL, opacity: 1)
     )
     
+    /// Adjust filter
     public var adjustment:IMPWBAdjustment!{
         didSet{
             self.updateBuffer(&adjustmentBuffer, context:context, adjustment:&adjustment, size:sizeof(IMPWBAdjustment))
@@ -26,6 +33,10 @@ public class IMPWBFilter:IMPFilter,IMPAdjustmentProtocol{
     public var adjustmentBuffer:MTLBuffer?
     public var kernel:IMPFunction!
     
+    ///  Create WB filter.
+    ///
+    ///  - parameter context: device context
+    ///
     public required init(context: IMPContext) {
         super.init(context: context)
         kernel = IMPFunction(context: self.context, name: "kernel_adjustWB")
